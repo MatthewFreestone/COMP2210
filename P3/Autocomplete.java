ae818273ed159b178ffd72f894b492574e81d12a
Matthew Freestone <matthewfreestone1593@gmail.com>
2021-02-23 14:36:01 -0600

    Module 3 hands-on

---
+++ P3/Autocomplete.java
@@ -0,0 +1,15 @@
+public class Autocomplete {
+
+	/**
+	 * Initializes a data structure from the given array of terms.
+	 * This method throws a NullPointerException if terms is null.
+	 */
+	public Autocomplete(Term[] terms) { }
+
+	/** 
+	 * Returns all terms that start with the given prefix, in descending order of weight. 
+	 * This method throws a NullPointerException if prefix is null.
+	 */
+	public Term[] allMatches(String prefix) { }
+
+}
---
+++ P3/BinarySearch.java
@@ -0,0 +1,16 @@
+public class BinarySearch {
+
+    /**
+     * Returns the index of the first key in a[] that equals the search key, 
+     * or -1 if no such key exists. This method throws a NullPointerException
+     * if any parameter is null.
+     */
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) { }
+
+    /**
+     * Returns the index of the last key in a[] that equals the search key, 
+     * or -1 if no such key exists. This method throws a NullPointerException
+     * if any parameter is null.
+     */
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) { }
+}
