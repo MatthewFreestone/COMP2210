378f9accf26d879ff0e17c1def2de9439c042b7e
Matthew Freestona <matthewfreestone1593@gmail.com>
2021-03-23 06:16:31 -0500

    first attempt at P5

---
+++ P5/ExampleGameClient.java
@@ -0,0 +1,20 @@
+public class ExampleGameClient {
+public static void main(String[] args) {
+    WordSearchGame game = WordSearchGameFactory.createGame();
+    game.loadLexicon("words.txt");
+    game.setBoard(new String[]{"E", "E", "C", "A", "A", "L", "E", "P", "H", 
+                                "N", "B", "O", "Q", "T", "T", "Y"});
+    System.out.print("LENT is on the board at the following positions: ");
+    System.out.println(game.isOnBoard("LENT"));
+    System.out.print("POPE is not on the board: ");
+    System.out.println(game.isOnBoard("POPE"));
+    System.out.println("All words of length 6 or more: ");
+    System.out.println(game.getAllScorableWords(6));
+}
+}
+/*
+LENT is on the board at the following positions: [5, 6, 9, 14]
+POPE is not on the board: []
+All words of length 6 or more: 
+[ALEPOT, BENTHAL, PELEAN, TOECAP]
+*/
